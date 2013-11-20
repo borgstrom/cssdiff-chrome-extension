c671fb712ef754e200f5e8522f1119b55eb7c5b6
Jon Davey <jon@criticalflicker.com>
2013-11-20 15:52:42 -0500

    initial commit

---
+++ panel.js
@@ -0,0 +1,6 @@
+chrome.devtools.panels.create(
+    'CSS Record',
+    null, // No icon path
+    'panel/record_panel.html',
+    null // no callback needed
+);
---
+++ panel/record_panel.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	$('button').click(addRecordListener);
+
+});
+
+function addRecordListener(e) {
+	 console.log("Hello, world!")
+	// get the current styles of the page
+	$(e.target).toggleClass("recording");
+	chrome.devtools.inspectedWindow.getResources(function(resources) {
+		$("#before").val("");
+		for (var i = 0; i <= resources.length; i++) {
+			if (resources[i].type == "stylesheet") {
+				$("#before").val(resources[i].type);
+				var c = resources[i].getContent(function(content, encoding){
+		    		$("#before").val(content);	
+		    		// if (!$(e.target).hasClass("recording")) {
+		    		// 	$("#after").val(content);	
+
+		    		// } else {
+		    		// 	$("#after").val("");
+		    		// }
+				});
+				break;
+			}
+			
+			
+		}
+	});
+	// listen for a inspector style change
+	chrome.devtools.inspectedWindow.onResourceContentCommitted.addListener(function(resource, content) {
+	    $("#after").val("");
+	    $("#after").val(content);
+	})
+}
