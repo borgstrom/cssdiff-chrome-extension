e6a8bf0548f135b451e283abb5751933863485f5
Jon Davey <jon@criticalflicker.com>
2013-11-21 18:20:28 -0500

    Added jsdifflib library.

--- panel/record_panel.js
+++ panel/record_panel.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-	$('button').click(addRecordListener);
+	$('.record').click(addRecordListener);
+	$('.differ').click(diffUsingJS);
 
 });
 
@@ -14,7 +15,10 @@ function addRecordListener(e) {
 				$("#before").val(resources[i].type);
 				var c = resources[i].getContent(function(content, encoding){
 		    		$("#before").val(content);	
+		    		//alert(content);
+		    		var str = String(content).substring(0,500);
 					chrome.devtools.inspectedWindow.eval('console.log("content:'+"hello content"+'")');
+					chrome.devtools.inspectedWindow.eval('console.log("content:'+str+'")');
 		    		// if (!$(e.target).hasClass("recording")) {
 		    		// 	$("#after").val(content);	
 
@@ -35,3 +39,29 @@ function addRecordListener(e) {
 	    $("#after").val(content);
 	})
 }
+
+
+function diffUsingJS() {
+	"use strict";
+	var byId = function (id) { return document.getElementById(id); },
+		base = difflib.stringAsLines(byId("before").value),
+		newtxt = difflib.stringAsLines(byId("after").value),
+		sm = new difflib.SequenceMatcher(base, newtxt),
+		opcodes = sm.get_opcodes(),
+		diffoutputdiv = byId("diffoutput"),
+		contextSize = 0	;
+
+	diffoutputdiv.innerHTML = "";
+	contextSize = contextSize || null;
+
+	diffoutputdiv.appendChild(diffview.buildView({
+		baseTextLines: base,
+		newTextLines: newtxt,
+		opcodes: opcodes,
+		baseTextName: "Bafore",
+		newTextName: "After",
+		contextSize: contextSize,
+		viewType: "sidebyside"
+	}));
+}
+
