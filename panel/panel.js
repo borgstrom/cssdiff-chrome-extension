a193948cd644b310dbaa5be86571db38e2cd980e
Evan Borgstrom <evan@fatbox.ca>
2013-11-30 22:29:29 -0500

    jsdifflib is overkill for what we need, revert to diff.js & prettify

--- panel/panel.js
+++ panel/panel.js
@@ -8,24 +8,7 @@ function byId(id) {
 
 (function(window, document, undefined) {
     var output = byId('output'),
-        template_source = byId('output-template').innerHTML,
-        context_size = 10; // TODO: make this a configuration option
-
-    function build_diff(resource1, resource2) {
-        var r1_lines = difflib.stringAsLines(resource1),
-            r2_lines = difflib.stringAsLines(resource2),
-            matcher = new difflib.SequenceMatcher(r1_lines, r2_lines);
-
-        return diffview.buildView({
-            viewType: 1,
-            contextSize: context_size,
-            opcodes: matcher.get_opcodes(),
-            baseTextLines: r1_lines,
-            baseTextName: 'Initial',
-            newTextLines: r2_lines,
-            newTextName: 'Current',
-        }).outerHTML;
-    }
+        template_source = byId('output-template').innerHTML;
 
     // we attach a function to our window object that will be called whenever
     // our panel is shown (see devtools.js)
@@ -36,12 +19,16 @@ function byId(id) {
 
         for (url in modified_resources) {
             context['resources'].push({
-                'url': url,
-                'diff': build_diff(initial_resources[url], modified_resources[url])
+                'diff': JsDiff.createPatch(
+                    url,
+                    initial_resources[url],
+                    modified_resources[url]
+                )
             });
         }
 
         output.innerHTML = Mustache.render(template_source, context);
+        prettyPrint()
     }
 
 })(window, document);
